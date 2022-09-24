9fa87428e239f90858310bf77c168ef70a4e6d85
wonjoon <zzzdnjswns@naver.com>
2022-09-25 01:16:53 +0900

    graphQL

--- api/src/index.js
+++ api/src/index.js
@@ -2,9 +2,31 @@
 // This is the main entry point of our application
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 4000;
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello Web Server!!!');
 });
 
-app.listen(4000, () => console.log('Listening on port 4000!'));
+app.listen(port, () =>
+  console.log(
+    `GraphQL Server runine at http://localhost:${port}${server.graphqlPath}`
+  )
+);
