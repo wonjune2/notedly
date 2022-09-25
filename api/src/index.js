cf055825f4492aeea956f8870d3af4b742ba0feb
wonjoon <zzzdnjswns@naver.com>
2022-09-26 00:04:25 +0900

    query mutation

--- api/src/index.js
+++ api/src/index.js
@@ -5,15 +5,48 @@ const app = express();
 const port = process.env.PORT || 4000;
 const { ApolloServer, gql } = require('apollo-server-express');
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 const typeDefs = gql`
   type Query {
     hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
+    hello: () => 'Hello World!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
   }
 };
 
